8bee2266012faf7162411b4f13005f9e1c2e784e
Mercedes Marighetti <anamercedes.marighetti@bbva.com>
2024-11-11 17:41:51 -0300

    EjercicioDB 11-11

--- exceptionHandlers/Lista.py
+++ exceptionHandlers/Lista.py
@@ -1,10 +1,13 @@
+
+#Definicion del error custom ##################################
 class ElementoDuplicadoError(ValueError):
     def __init__(self, mensaje):
         self.mensaje = mensaje
         super().__init__(self.mensaje)
 
     def __str__(self):
-        return f'{self.mensaje}'
+        return f'Aca hay un error: {self.mensaje} y de clase: {self.__class__}'
+################################################################
 
 def agregar_una_vez(lista, elem) -> ElementoDuplicadoError:
     if elem in lista:
@@ -12,23 +15,17 @@ def agregar_una_vez(lista, elem) -> ElementoDuplicadoError:
     else:
         lista.append(elem)
 
+################################################################
 def main():
     listaOriginal = [1, 5, -2]
     agregar = [10, -2, "Hola", 7, "Arroz"]
     sumar = [10, 2, -7, 3]
-
-    for valor in agregar:
+    
+    for valor in agregar + sumar: #Agregar + sumar
         try:
             agregar_una_vez(listaOriginal, valor)
         except ElementoDuplicadoError as e:
             print(f'{e}, {e.__class__}')
-            
-    for valor in sumar:
-        try:
-            agregar_una_vez(listaOriginal, valor)
-        except ValueError as e:
-            print(f'{e}')
-            
 
 
     print("Elementos de la lista:", listaOriginal)
